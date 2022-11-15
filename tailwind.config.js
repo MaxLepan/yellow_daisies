b374a3387460e361c52b6b8bf15891656deadb0c
LucieEditionsTissot <llesnier@editions-tissot.fr>
2022-11-15 11:30:12 +0100

    problem to deploy tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  prefix: '',
+  content: [
+    './public/**/*.{html,ts,css,scss}',
+    './public/**/**/*.{html,ts,css,scss}',
+
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '360px',
+        sm: '640px',
+        md: '768px',
+        lg: '1025px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  variants: {
+    extend: {
+    },
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  }
+};
