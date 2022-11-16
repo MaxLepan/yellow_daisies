003eec6d4fa7fdc8be685f50c77b2571196c504f
MaxLepan <max.lepan@laposte.net>
2022-11-16 10:52:20 +0100

    Added the decade to the sorting function

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,15 +1,17 @@
 import daisies from '../bellis.json' assert { type: "json" };
 
-function getCount() {
+function getCount(decade) {
     let obj = {};
     for (let i = 0, j = daisies.length; i < j; i++) {
-        if (obj[daisies[i].region]) {
-            obj[daisies[i].region]++;
-        }else{
-            obj[daisies[i].region] = 1;
+        if (daisies[i].decennie === decade) {
+            if (obj[daisies[i].region]) {
+                obj[daisies[i].region]++;
+            } else {
+                obj[daisies[i].region] = 1;
+            }
         }
     }
     return obj;
 }
 
-console.log(getCount());
+console.log(getCount(1990));
