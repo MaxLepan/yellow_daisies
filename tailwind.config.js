992eca8aab34a84e9a27ecc37ee85b07d21555ac
Lucie Lesnier <llesnier@intra.weka.fr>
2022-11-16 21:59:54 +0100

    refacto tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,31 @@
 module.exports = {
-  prefix: 'page-',
-  content: [
-    './public/index.html',
-    './public/**/*.{html,js}',
-  ],
+    prefix: 'page-',
+    purge: [
+            './public/index.html',
+            './public/**/*.{html,js}',
+        ],
 
-
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Bubble M', ...defaultTheme.fontFamily.sans],
-      },
-      screens: {
-        xs: '360px',
-        sm: '640px',
-        md: '768px',
-        lg: '1025px',
-        xl: '1280px',
-        '2xl': '1536px',
-      },
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Bubble M', ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                xs: '360px',
+                sm: '640px',
+                md: '768px',
+                lg: '1025px',
+                xl: '1280px',
+                '2xl': '1536px',
+            },
+        },
     },
-  },
-  variants: {
-    extend: {
+    variants: {
+        extend: {},
     },
-  },
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-  }
+    plugins: [],
+    corePlugins: {
+        preflight: false,
+    }
 };
