324c1d378b178998001d4f4b6158b53cb5b00c50
LucieEditionsTissot <llesnier@editions-tissot.fr>
2022-11-16 09:10:57 +0100

    problem to set height to svg

--- public/scripts/d3.js
+++ public/scripts/d3.js
@@ -1,11 +1,32 @@
+const data = d3.json("bellis.json");
+const width = 200,
+    scaleFactor = 10,
+    barHeight = 20;
 
-async function csvParse(d3) {
-    await d3.csv("./../BELLIS-VS-ERIGERON.csv", function (data) {
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i].nomScientifiqueRef);
-        }
-        console.log(data);
-    })
-}
+const graph = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", barHeight * data.length);
+
+const bar = graph.selectAll("g")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("transform", function (d, i) {
+        return "translate(0," + i * barHeight + ")";
+    });
+
+bar.append("rect")
+    .attr("width", function(d) {
+    return d * scaleFactor;
+})
+    .attr("height", barHeight - 1);
+
+    bar.append("text")
+    .attr("x", function(d) { return (d*scaleFactor); })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) { return d; });
+
 
 
